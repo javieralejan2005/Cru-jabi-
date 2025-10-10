bbbe430a162441d755bb944b2cebee159184ca09
Julitocrack <juliojeff10@hotmail.com>
2025-10-10 03:31:40 +0000

    Agregue todos los metodos necesarios.

--- modelo/productodao.py
+++ modelo/productodao.py
@@ -24,4 +24,67 @@ class ProductoDAO:
 
         self.bd.cerrarConexionBD()
 
+    def guardarProducto(self):
+
+        # Abrir la conexión a la base de datos  
+
+        self.bd.establecerConexionBD()
+
+        cursor = self.bd.conexion.cursor()
+        sp = "exec [dbo].[sp_guardar_producto] @clave=?, @descripción=?, @existencia?, @precio=?"
+        parametros = (self.producto.clave, self.producto.descripcion, self.producto.existencia, self.producto.precio)
+        cursor.execute(sp, parametros)
+        self.bd.conexion.commit()
+
+        # Cerrar la conexión a la base de datos
+
+        self.bd.cerrarConexionBD()
+
+    def actualizarProducto(self):
+
+        # Abrir la conexión a la base de datos  
+
+        self.bd.establecerConexionBD()
+
+        cursor = self.bd.conexion.cursor()
+        sp = "exec [dbo].[sp_actualizar_producto] @idProducto=?, @clave=?, @descripción=?, @existencia?, @precio=?"
+        parametros = (self.producto.idProducto, self.producto.clave, self.producto.descripcion, self.producto.existencia, self.producto.precio)
+        cursor.execute(sp, parametros)
+        self.bd.conexion.commit()
+
+        # Cerrar la conexión a la base de datos
+
+        self.bd.cerrarConexionBD()
+
+    def eliminarProducto(self):
+
+        # Abrir la conexión a la base de datos  
+
+        self.bd.establecerConexionBD()
+
+        cursor = self.bd.conexion.cursor()
+        sp = "exec [dbo].[sp_eliminar_producto] @idProducto=?"
+        parametros = (self.producto.idProducto)
+        cursor.execute(sp, parametros)
+        self.bd.conexion.commit()
+
+        # Cerrar la conexión a la base de datos
+
+        self.bd.cerrarConexionBD()
+
+    
+    def contarProductos(self):
+
+        self.bd.establecerConexionBD()
+
+        cursor = self.bd.conexion.cursor()
+        sp = "exec [dbo].[sp_contar_productos]"
+        cursor.execute(sp)
+        resultado = cursor.fetchone()
+
+        print(f"Total de productos: {resultado[0]}")
+
+        self.bd.cerrarConexionBD()
+
+
 
